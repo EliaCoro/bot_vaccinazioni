ffed47eb1a8bb9160fe5531c900c3316191e96d1
Elia <elia.coro02@gmail.com>
2021-06-04 22:44:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 
-
 let namePlaces = [
     "La cittadella",
     "Chioggia",
@@ -14,24 +13,6 @@ let namePlaces = [
     "Venezia - Pala Expo",
     "Venezia - Rampa santa chiara"
 ];
-
-let mouth = ["gennaio",
-    "febbraio",
-    "marzo",
-    "aprile",
-    "marzo",
-    "aprile",
-    "maggio",
-    "giugno",
-    "luglio",
-    
-    "agosto",
-    "settembre",
-    "ottobre",
-    "novembre",
-    "dicembre"
-];
-
 
 const user = {
     name: "",
@@ -53,21 +34,22 @@ const user = {
         true, //Venezia - Pala Expo
         true //Venezia - Rampa santa chiara
     ],
-    maxDate: new Date("2020/06/10")
+    maxDate: new Date("2020/07/30")
 };
 
 const ulssLink = "https://vaccinicovid.regione.veneto.it/ulss3";
 
 (async () => {
-    var reservation = true;
-
+    let reservation = true;
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    let page;
+    let isAvailable;
     while (reservation) {
         try {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
-            });
-            const page = await browser.newPage();
-            await page.setDefaultNavigationTimeout(0); 
+            page = await browser.newPage();
+            await page.setDefaultNavigationTimeout(0);
             await page.goto(ulssLink);
 
             // PRIMA PAGINA
@@ -75,75 +57,75 @@ const ulssLink = "https://vaccinicovid.regione.veneto.it/ulss3";
             await page.type('#num_tessera', user.idFiscalCode);
             await page.click('input[type="checkbox"]');
             await page.click('.btn-primary');
-
             console.log("[1] - Dati inseriti correttamente");
-            var isAvailable;
-            setTimeout(async () => {
-                await page.click('button[onclick="scegliserv(178)"]');
+            
+            await page.waitForTimeout(800)
+            await page.click('button[onclick="scegliserv(178)"]');
+            await page.waitForTimeout(800)
+            for (let i = 0; i < 11; i++) {
+                if (user.places[i]) {
+                    isAvailable = (await page.$('button.btn.btn-primary.btn-full:nth-child(' + (i + 3) + ')[disabled]')) == null;
+                    if (isAvailable) {
+                        await page.click('.btn-full:nth-child(' + (i + 3) + ')');
+                        console.log("[2] - Trovata una prenotazione libera a " + namePlaces[i]);
+                        await page.waitForTimeout(500)
+                        try {
+                            // Prende tutte le date disponibili
+                            let dates = await page.$$('td.highlight');
 
-                setTimeout(async () => {
-                    for (let i = 0; i < 11; i++) {
-                        if (user.places[i]) {
-                            isAvailable = (await page.$('button.btn.btn-primary.btn-full:nth-child(' + (i + 3) + ')[disabled]')) == null;
-                            if (isAvailable) {
-                                await page.click('.btn-full:nth-child(' + (i + 3) + ')');
-                                console.log("[2] - Trovata una prenotazione libera a " + namePlaces[i]);
-                                setTimeout(async () => {
-                                    try {
-                                        // Prende tutte le date disponibili
-                                        let dates = await page.$$('td.highlight');
-
-                                        // Se non ci sono date l'esecuzione del programma viene arrestata
-                                        if (dates == [] || dates.length == 0) {
-                                            //await page.click('button[aria-label="next"]');
-                                            //dates = await page.$$('td.highlight');
-                                            //if (dates == [] || dates.length == 0) 
-                                            throw ("Non ci sono date");
-                                        }
-
-                                        // Viene selezionata la prima data disponibile
-                                        /*await page.screenshot({
-                                            fullPage: true,
-                                            path: getRandomInt(2) + 'confermaReservation.png'
-                                        });*/
-
-                                        let hrefElement = await page.$('td.highlight:nth-child(1)');
-                                        if (hrefElement == null || hrefElement == undefined)
-                                            hrefElement = await page.$("td.highlight");
-                                        console.log(hrefElement)
-                                        await hrefElement.click();
-
-                                        setTimeout(async () => {
-                                            console.log("[3] - Giorno selezionato");
-                                            await page.click('.btn-full');
-
-                                            setTimeout(async () => {
-                                                await page.type('input[name="cognome"]', user.surname);
-                                                await page.type('input[name="nome"]', user.name);
-                                                await page.type('input[name="email"]', user.email);
-                                                await page.type('input[name="cellulare"]', user.phone);
-                                                await page.screenshot({
-                                                    fullPage: true,
-                                                    path: 'resume/' + user.name + user.surname + '_resume.png'
-                                                })
-                                            }, 800);
-                                        }, 800);
-
-                                    } catch (error) {
-                                        console.log("Non ci sono date disponibili per questo mese.")
-                                    }
-                                }, 800);
+                            // Se non ci sono date l'esecuzione del programma viene arrestata
+                            if (dates == [] || dates.length == 0) {
+                                await page.click('button[aria-label="next"]');
+                                dates = await page.$$('td.highlight');
+                                if (dates == [] || dates.length == 0)
+                                    throw ("Non ci sono date");
                             }
+
+                            // Viene selezionata la prima data disponibile
+                            let hrefElement = await page.$('td.highlight:nth-child(1)');
+                            let attr = await page.$$eval("td.highlight:nth-child(1)", el => el.map(x => x.getAttribute("data-date")));
+
+                            if (hrefElement == null || hrefElement == undefined) {
+                                hrefElement = await page.$("td.highlight");
+                                attr = await page.evaluate(el => el.getAttribute("data-date"), hrefElement);
+                            }
+                            if ((new Date(attr[0])).getTime() >= user.maxDate.getTime()) {
+                                await hrefElement.click();
+                                await page.waitForTimeout(500)
+                                console.log("[3] - Giorno selezionato");
+                                await page.click('.btn-full');
+                                await page.waitForTimeout(500)
+                                await page.type('input[name="cognome"]', user.surname);
+                                await page.type('input[name="nome"]', user.name);
+                                await page.type('input[name="email"]', user.email);
+                                await page.type('input[name="cellulare"]', user.phone);
+                                await page.screenshot({
+                                    fullPage: true,
+                                    path: 'resume/' + user.name + user.surname + '_resume.png'
+                                })
+                                await page.click('#bottoneconferma');
+                                await page.waitForTimeout(500)
+                                    await page.screenshot({
+                                        fullPage: true,
+                                        path: 'resume/' + user.name + user.surname + '_resume.png'
+                                    })
+                                reservation = false;
+                            }
+                        } catch (error) {
+                            console.log("Non ci sono date disponibili per questo mese.")
                         }
                     }
-                }, 800);
-            }, 800);
-
-
+                }
+            }
         } catch (error) {
+            await page.screenshot({
+                fullPage: true,
+                path: 'error/' + 'error.png'
+            })
             throw (error);
         }
-        //await browser.close();
+        //await page.waitForTimeout(800)
+        await page.close();
         console.log("=========================================================");
     }
     await browser.close();
